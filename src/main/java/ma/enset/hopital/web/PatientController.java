98e6b45641170af4b8271cf96c6501c793f2fd7e
Amyn617 <aarfaoui617@gmail.com>
2025-03-17 11:13:37 +0000

    Ajout de pagination

--- src/main/java/ma/enset/hopital/web/PatientController.java
+++ src/main/java/ma/enset/hopital/web/PatientController.java
@@ -3,20 +3,26 @@ package ma.enset.hopital.web;
 import lombok.AllArgsConstructor;
 import ma.enset.hopital.entities.Patient;
 import ma.enset.hopital.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @AllArgsConstructor
 public class PatientController {
     private PatientRepository patientRepository;
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patientList = patientRepository.findAll();
-        model.addAttribute("listPatients", patientList);
+    public String index(Model model, 
+                       @RequestParam(name = "page", defaultValue = "1") int page,
+                       @RequestParam(name = "size", defaultValue = "5") int size) {
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", pagePatients.getTotalPages());
         return "patients";
     }
 }
